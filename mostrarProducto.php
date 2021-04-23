643de78375f43808af35d45ca07adffacc0b6040
Yacdany <jack20-21@hotmail.com>
2021-04-22 19:21:48 -0600

    Se muestran los productos seleccionados

--- mostrarProducto.php
+++ mostrarProducto.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,27 +8,42 @@
     <link rel="stylesheet" href="css/bootstrap-5.0.0/css/bootstrap.min.css">
     <title>Productos</title>
 </head>
+
 <body>
     <?php
-        include_once 'layouts/header.php';
-        include_once 'db/config.php';
-        include_once 'php/functions.php';
+    include_once 'layouts/header.php';
+    include_once 'db/config.php';
+    include_once 'php/functions.php';
 
-        if(isset($_GET['id'])){
-            
-        }else{
-            header('Location:productos.php');
-        }
+    if (isset($_GET['id'])) {
+        $row=mostrarProductoSeleccionado($conexion,$_GET['id']);
+    } else {
+        header('Location:productos.php');
+    }
     ?>
 
     <div class="container">
         <div class="row">
-            <div class="col">
-
+            <div class="col-md-12 txt-center mt-5">
+            <?php
+                foreach ($row as $key => $value) :
+            ?>
+                <div class="card" style="width: 18rem;">
+                    <img src="img/productos/<?php echo $value->imagen;?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $value->nombre;?></h5>
+                      <!--  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                       --> <a href="#" class="btn btn-primary">Comprar</a>
+                    </div>
+                </div>
+                <?php
+                    endforeach;
+                ?>
             </div>
         </div>
     </div>
 
     <script src="css/bootstrap-5.0.0/js/bootstrap.bundle.js"></script>
 </body>
+
 </html>
--- php/functions.php
+++ php/functions.php
@@ -15,5 +15,22 @@
         return $registros;
     }
 
+    function mostrarProductoSeleccionado($conexion,$id){
+        $producto=array();
+        $id_producto=$id;
+        
+        $sql="SELECT * FROM productos WHERE id=:id_producto";
+
+        $result=$conexion->prepare($sql);
+        $result->bindParam(':id_producto',$id_producto,PDO::PARAM_INT);
+        $result->execute();
+
+        while($row=$result->fetchObject()){
+            $producto[]=$row;
+        }
+
+        return $producto;
+    }
+
 //producto seleccionado
 ?>
